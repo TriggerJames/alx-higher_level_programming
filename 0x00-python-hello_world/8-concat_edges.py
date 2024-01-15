2c448bfef5a315dda696ffbc3bccf1a4de6b4d9d
TriggerJames <Kimothojames254@gmail.com>
2024-01-16 01:41:50 +0300

    0x00. Python
    Hello, World

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-print(str{69:98})
+str = "Python is an interpreted, interactive, object-oriented programming" \
+      " language that combines remarkable power with very clear syntax"
+print(str[41:66] + str[106:112] + str[0:6])
