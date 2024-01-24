a13d40c7adcbb508d353494aef8708874210ff2c
TriggerJames <Kimothojames254@gmail.com>
2024-01-24 21:42:28 +0300

    0x02. Python
    Import & Modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,11 +4,15 @@ def magic_calculation(a, b):
     add = magic_calculation_102.add
     sub = magic_calculation_102.sub
     
+    # Check if a is less than b
     if a < b:
         c = add(a, b)
+        
+        # Sum the numbers from 4 to 6
         for i in range(4, 6):
             c = add(c, i)
+        
         return c
     else:
+        # If a is not less than b, perform subtraction
         return sub(a, b)
-
