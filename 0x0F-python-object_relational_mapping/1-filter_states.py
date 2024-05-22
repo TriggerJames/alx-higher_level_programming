9c600f22f9484bb333b13dc9625c61fff4b76483
TriggerJames <Kimothojames254@gmail.com>
2024-05-22 13:31:21 +0300

    0x0F. Python
    Object-Relational Mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+
+"""Lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
     from sys import argv
